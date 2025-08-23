0ed990ecbaeb25f479768402fcbbb09b23a88476
Egrenbido Oyintare Paul <73933865+OyintarePaul@users.noreply.github.com>
2025-08-23 21:27:43 +0000

    added new skill

--- constants/index.ts
+++ constants/index.ts
@@ -90,7 +90,7 @@ export const skillCategories = [
     skills: [
       "HTML",
       "CSS",
-      "JavaScript",
+      "JavaScript/TypeScript",
       "React",
       "React Native",
       "Next.js",
